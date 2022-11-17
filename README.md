151b403530f6161a4dcab9115dc45765baa054b6
mattdig <mattdigiuseppe@gmail.com>
2022-11-17 15:51:19 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# brightspace-console
+A web-based CLI for common D2L Brightspace admin functions and API access. Based on JQuery Console.
+
+See https://github.com/chrisdone/jquery-console for info about the console.
+
+To install in Brightspace:
+Download the ZIP file
+Create a new folder within the /shared/ directory of Brightspace's Public Files.
+Upload the brightspace-console.zip file to it and unzip
+
+Creating a link to the Console:
+In the Navigation and Themes admin tool, create a new Custom link
+URL: https://yoursite.brightspace.com/shared/path-to-console/console.html?ou={OrgUnitId}
+Important: you need to use the full URL, a relative path starting with /shared won’t work
+Set it to open in the Same Window
+Set availability for your admin level roles as appropriate
+Add the custom link to a navbar
---
+++ js/main.js
@@ -0,0 +1,297 @@
+const params = new Proxy(new URLSearchParams(window.location.search), { get: (searchParams, prop) => searchParams.get(prop) });
+const WORKING_OU = params.ou;
+const bs = new Brightspace(WORKING_OU);
+let BASE_OU = false;
+
+bs.get('/d2l/api/lp/(version)/organization/info').then((response) => {
+    BASE_OU = response.Identifier;
+});
+
+let CONSOLE;
+
+
+$(document).ready(function () {
+    /* Fifth console */
+    let user_commands = ["impersonate", "enrollment", "user", 'api'];
+    user_commands.sort();
+    let all_commands = ['help'].concat(user_commands);
+    
+    CONSOLE = $('#console').console({
+        welcomeMessage: 'Welcome to the console! Type "help" to see the list of commands.\n&&uarr; or &&darr; to scroll through your command history. TAB to autocomplete commands.',
+        promptLabel: 'D2L> ',
+        commandHandle: async function (line) {
+
+            if (line) {
+
+                // trim line and remove double spaces
+                line = line.trim().replace(/\s+/g, ' ');
+
+                //console.log('"' + line + '"');
+
+                let m = 'Command not recognized';
+                let c = 'jquery-console-message-value';
+                let response;
+
+                let commandParts = line.split(' ');
+
+                if (commandParts[0] == "help") {
+                    m = "Supported commands: " + user_commands.join(", ");
+                } else if (commandParts[0] == "impersonate") {
+                    if (commandParts.length >= 2) {
+
+                        let flag = '';
+                        let identifier = commandParts[1];
+
+                        if (commandParts.length > 2 && commandParts[1] == '-i') {
+                            flag = commandParts[1] + ' ';
+                            identifier = commandParts[2];
+                        }
+
+                        m = "Impersonnating " + identifier + '... This page will reload if successful.';
+                        response = await impersonate(flag + identifier);
+                        m += "\n" + response;
+
+                    } else {
+                        m = "Usage: impersonate <username|email|banner-id>\nor\nimpersonate -i <user-id>";
+                    }
+                } else if (commandParts[0] == 'enrollment' || commandParts[0] == 'enrolment') {
+                    if (commandParts.length >= 2) {
+
+                        let flag = '';
+                        let identifier = commandParts[1];
+
+                        if (commandParts.length > 2 && commandParts[1] == '-i') {
+                            flag = commandParts[1] + ' ';
+                            identifier = commandParts[2];
+                        }
+
+                        let response = await enrollment_status(flag + identifier);
+
+                        m = response;
+
+                    } else {
+                        m = "Usage: enrollment <username|email|banner-id>\nor\nenrollment -i <user-id>";
+                    }
+                } else if (commandParts[0] == 'user') {
+
+                    if (commandParts.length >= 2) {
+                        let flag = '';
+                        let identifier = commandParts[1];
+
+                        if (commandParts.length > 2 && commandParts[1] == '-i') {
+                            flag = commandParts[1] + ' ';
+                            identifier = commandParts[2];
+                        }
+
+                        let response = await user_info(flag + identifier);
+
+                        if (typeof response == 'object') {
+
+                            m = response.FirstName + " " + response.LastName + " (" + response.UserName + " | " + response.OrgDefinedId + "):\n";
+
+                            let orgRole = await bs.get('/d2l/api/lp/(version)/enrollments/users/' + response.UserId + '/orgunits/' + BASE_OU);
+
+                            let roleData = await bs.get('/d2l/api/lp/(version)/roles/' + orgRole.RoleId);
+
+                            m += "Org-level Role: " + roleData.DisplayName + "\n";
+
+                            m += JSON.stringify(response, null, 2);
+                        } else {
+                            m = 'Error: User not found';
+                        }
+
+                    } else {
+                        m = "Usage: user <username|email|banner-id>\nor\nuser -i <user-id>";
+                    }
+                } else if (commandParts[0] == 'api') {
+
+                    if (commandParts.length == 3) {
+                        let method = commandParts[1];
+                        let url = commandParts[2];
+                        let data = null;
+                        if (method == 'put' || method == 'post' || method == 'submit') {
+                            let json = $('#json_input').val();
+                            data = JSON.parse(json);
+                        }
+
+                        m = "Calling " + method + " " + url + "\n";
+                        m += "See \"JSON Output\" for result. ";
+                        api_call(method, url, data);
+
+                    } else {
+                        m = "Usage: api <method> <url>\nUse the \"JSON Input\" field to input data\n";
+                        m += ' <<a href="https://docs.valence.desire2learn.com/reference.html">>Brightspace API Reference<</a>>';
+                    }
+
+                } else {
+                    c = 'jquery-console-message-error';
+                }
+
+                //console.log(m);
+
+                return [{ msg: m, className: c }];
+            } else {
+                return [{ msg: '', className: "jquery-console-message-value" }];
+            }
+        },
+        commands: all_commands,
+        completeHandle: function (prefix) {
+            let commands = all_commands;
+            let ret = [];
+            for (let i = 0; i < commands.length; i++) {
+                let command = commands[i];
+                if (command.lastIndexOf(prefix, 0) === 0) {
+                    ret.push(command.substring(prefix.length) + ' ');
+                }
+            }
+            return ret;
+        },
+        promptHistory: true,
+        storeHistory: true,
+        autofocus: true
+    });
+});
+
+
+
+async function impersonate(identifier) {
+
+    let userId = await getUserId(identifier);
+
+    if (!userId) {
+        return "Error: User not found";
+    }
+
+    let url = "/d2l/lp/manageUsers/rpc/rpc_functions.d2lfile?ou=" + BASE_OU + "&d2l_rh=rpc&d2l_rt=call";
+
+    let data = {
+        d2l_rf: 'ImpersonateUser',
+        params: '{"param1":"' + userId + '","param2":"131"}',
+        d2l_action: 'rpc'
+    }
+
+    let response = await bs.submit(url, data);
+
+    if (response && response.Result !== undefined && (response.Result === null || response.Result[0] == "success")) {
+        window.open("/d2l/home/" + (WORKING_OU || ''), '_blank').focus();
+        return "Success! Reloading page...";
+    } else {
+        return 'Error: impersonnation failed';
+    }
+
+}
+
+async function enrollment_status(identifier) {
+
+    let courses = '';
+
+    let user = await user_info(identifier);
+
+    if (!user)
+        return 'Error: user not found';
+
+    courses += '<<a href="/d2l/lp/manageUsers/admin/newedit_enrollmentLog.d2l?ou=' + BASE_OU + '&d2l_isfromtab=1&uid=' + user.UserId + '">>' +
+        'View Enrollment Log for ' + user.FirstName + ' ' + user.LastName + '<</a>>\n\n';   
+
+    let url = "/d2l/api/lp/(version)/enrollments/users/" + user.UserId + "/orgUnits/";
+
+    let response = await bs.get(url);
+
+    if (typeof response == 'string') {
+        response = JSON.parse(response);
+    }
+
+    if (response) {
+        let courseArray = [];
+        response.Items.forEach((course) => {
+            if (course.OrgUnit.Type.Id == 3) {
+                courseArray.push(' <<a href="/d2l/home/' + course.OrgUnit.Id + '">>' + course.OrgUnit.Name + " (" + course.OrgUnit.Id + ") - " + course.Role.Name + '<</a>>');
+            }
+        });
+
+        courses += courseArray.join('\n');
+    } else {
+        courses = 'Error: no courses found';
+    }
+
+    return courses;
+}
+
+async function user_info(identifier) {
+
+    identifier = identifier.split(' ');
+    let param = 'userName';
+
+    if (identifier.length == 2 && identifier[0] == '-i') {
+        param = 'userId';
+        identifier = identifier[1];
+    } else {
+        identifier = identifier[0];
+        if (identifier.indexOf('@') != -1) {
+            param = 'externalEmail';
+        } else if (!isNaN(identifier)) {
+            param = 'orgDefinedId';
+        }
+    }
+
+    let query = (param != 'userId' ? "?" + param + "=" + identifier : identifier);
+
+    let url = "/d2l/api/lp/(version)/users/" + query;
+
+    let response = await bs.get(url);
+
+    if (typeof response == 'string') {
+        response = JSON.parse(response);
+    }
+
+    if (response[0] !== undefined) {
+        response = response[0];
+    } else if (response.Error !== undefined) {
+        return false;
+    }
+
+    return response;
+
+}
+
+async function getUserId(identifier) {
+
+    let user = await user_info(identifier);
+
+    if(!user)
+        return false;
+
+    return user.UserId;
+}
+
+async function api_call(method, url, data) {
+
+    let response = { error: "Invalid method" };
+
+    switch (method) {
+
+        case 'get':
+            response = await bs.get(url);
+            break;
+
+        case 'post':
+            response = await bs.post(url, data);
+            break;
+
+        case 'put':
+            response = await bs.put(url, data);
+            break;
+
+        case 'delete':
+            response = await bs.delete(url);
+            break;
+
+        case 'submit':
+            response = await bs.submit(url, data);
+            break;
+
+    }
+
+    $('#json_output').val(JSON.stringify(response, null, 2));
+
+}
